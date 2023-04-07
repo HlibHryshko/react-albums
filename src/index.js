37717e1d73c3adefffd3e95f7ace6af0d0e7bfd8
Hlib Hryshko <glebg27@gmail.com>
2023-04-07 20:44:43 +0200

    thunks are added to the application to handle users

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import './index.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import App from './App';
 
-const el = document.getElementById("root");
+const el = document.getElementById('root');
 const root = createRoot(el);
 
 root.render(
