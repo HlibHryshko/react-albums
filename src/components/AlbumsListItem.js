06df45fb392c446e52b0ee80a51b46def81d57f2
Hlib Hryshko <glebg27@gmail.com>
2023-04-08 19:14:28 +0200

    remove from the database function is implemented + refetching with tags is implemented(not the most generic way)

--- src/components/AlbumsListItem.js
+++ src/components/AlbumsListItem.js
@@ -1,16 +1,23 @@
+import { useRemoveAlbumMutation } from "../store";
 import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 
 import { GoTrashcan } from "react-icons/go";
 
 const AlbumsListItem = ({ album }) => {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
   const header = (
-    <div>
-      <Button>
+    <>
+      <Button className='mr-2' loading={results.isLoading} onClick={handleRemoveAlbum}>
         <GoTrashcan />
       </Button>
       {album.title}
-    </div>
+    </>
   );
   return (
     <ExpandablePanel key={album.id} header={header}>
