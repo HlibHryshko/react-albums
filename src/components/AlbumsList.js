d91eefe80256eeb8fbaec4f3f13c375320a02aeb
Hlib Hryshko <glebg27@gmail.com>
2023-04-08 09:17:34 +0200

    a list of albums is displayed

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,11 +1,33 @@
 import { useFetchAlbumsQuery } from "../store";
+import ExpandablePanel from "./ExpandablePanel";
+import Skeleton from "./Skeleton";
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
   console.log(data, error, isLoading);
 
-  return <div>Albums for {user.name}</div>;
+  let content = isLoading ? (
+    <Skeleton times={3} />
+  ) : error ? (
+    <div>Error loading albums.</div>
+  ) : (
+    data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    })
+  );
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
