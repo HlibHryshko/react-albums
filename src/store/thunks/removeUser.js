a5e410d2c3a5b4fd2d4598c00fd01a5b7b32770b
Hlib Hryshko <glebg27@gmail.com>
2023-04-08 08:06:42 +0200

    resolving merge conflict

--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -2,11 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const removeUser = createAsyncThunk('users/remove', async (user) => {
-<<<<<<< HEAD
-  await axios.delete(`http://localhost:3005/users/${user.id}`);
-=======
   await axios.delete(`http://localhost:3001/users/${user.id}`);
->>>>>>> 93a4bc78a7f31a42c6205a2af8876c1402f5ce10
 
   return user;
 });
