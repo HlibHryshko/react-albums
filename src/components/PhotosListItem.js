b284b38620ad2d065b7898cb67d6a99d28171db7
Hlib Hryshko <glebg27@gmail.com>
2023-04-11 17:49:17 +0200

    a photo is displayed

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,5 +1,7 @@
 const PhotosListItem = ({ photo }) => {
-  return "PhotosListItem";
+  return <div className="relative m-2">
+    <img className="h-20 w-20" src={photo.url} alt="random pic"></img>
+  </div>;
 };
 
 export default PhotosListItem;
