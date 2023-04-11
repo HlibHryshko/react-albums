8c73b0d8f67ad622a6ccf2f7a872fd2b1ef0fbd8
Hlib Hryshko <glebg27@gmail.com>
2023-04-11 17:23:35 +0200

    fetch photos + add photo functionality is added

--- src/components/PhotosList.js
+++ src/components/PhotosList.js
@@ -1,5 +1,24 @@
+import { useAddAlbumMutation, useFetchPhotosQuery } from "../store";
+import Button from "./Button";
+
 const PhotosList = ({ album }) => {
-  return "PhotosList";
+  const {data, isFetching, error} = useFetchPhotosQuery(album);
+  const [addPhoto, addPhotoResults] = useAddAlbumMutation();
+
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  return (
+    <div>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Photos In {album.title}</h3>
+        <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default PhotosList;
