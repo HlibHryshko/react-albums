4256f4b6ea5bc7bd683a4ec2542f6c2793ce6dfd
Hlib Hryshko <glebg27@gmail.com>
2023-04-11 18:04:59 +0200

    displaying delete button, when the mouse is on the element

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,6 +1,12 @@
+import { GoTrashcan } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+
 const PhotosListItem = ({ photo }) => {
-  return <div className="relative m-2">
+  return <div className="relative cursor-pointer m-2">
     <img className="h-20 w-20" src={photo.url} alt="random pic"></img>
+    <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+      <GoTrashcan className="text-3xl" />
+    </div>
   </div>;
 };
 
